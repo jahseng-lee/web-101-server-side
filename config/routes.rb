6fd7d772933f7c5e3c4fb3490043ca06f722bb7e
Jah Seng Lee <jah@fluxfederation.com>
2018-04-03 11:17:40 +1200

    Make good_boys#index the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "good_boys#index"
+
   resources :good_boys
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
